434ec3914495daf9f814d6c52e9896ae90bb235e
flintttan <flintttan@gmail.com>
2026-04-21 05:02:36 +0000

    fix: shorten alembic revision ids

--- backend/alembic/versions/20260420_add_system_role_to_users.py
+++ backend/alembic/versions/20260420_add_system_role_to_users.py
@@ -1,6 +1,6 @@
 """add system role to users
 
-Revision ID: 20260420_add_system_role
+Revision ID: 20260420_sys_role
 Revises: f3b9c4d7e8a1
 Create Date: 2026-04-20 00:00:00.000000
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = "20260420_add_system_role"
+revision = "20260420_sys_role"
 down_revision = "f3b9c4d7e8a1"
 branch_labels = None
 depends_on = None
